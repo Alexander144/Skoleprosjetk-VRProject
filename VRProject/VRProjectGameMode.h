f330884dc866fa4c6de0df2f62c32916ff0240de
Alexander144 <Alexan2405@gmail.com>
2016-09-21 15:04:16 +0200

    Pushet koden

---
+++ VRProject/VRProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "VRProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRPROJECT_API AVRProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
